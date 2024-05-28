cab2134e5bfb868817c1db18cb3bdcfc5509a224
moffaty <goga459972@gmail.com>
2024-05-28 17:01:15 +0300

    конфиг mocha

---
+++ .mocharc.js
@@ -0,0 +1,5 @@
+// mocha config
+module.exports = {
+    timeout: 5000, // set the default timeout for test cases (milliseconds)
+    spec: ['test/*.js'], // specify the location of the test file
+};
